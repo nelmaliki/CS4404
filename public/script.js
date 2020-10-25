0161e10f0f3869e64978ed4c1a65a35c8eef7f6e
matthew7758 <entei678@gmail.com>
2020-10-25 01:13:42 -0400

    Added function that displays json from server side representing candidates and votes. TODO: Query from database and populate the arrays instead of using the predefined ones

--- public/script.js
+++ public/script.js
@@ -1,1 +1,35 @@
 //TODO: Make client-side js that reads json from server-side and puts voting results in a ul in the webpage
+const votesList = document.getElementById("votes")
+const votesForm = document.querySelector("form")
+
+function addNewVote(candidate, voteNum) {
+    const newListItem = document.createElement("li")
+    newListItem.innerText = candidate + '\t' + voteNum
+    votesList.appendChild(newListItem)
+}
+
+fetch("/votes")
+    .then(res=>res.json())
+    .then(vvvvvvv => {
+        let candidates = vvvvvvv.nameCandidate
+        let votes = vvvvvvv.numVotes
+        //remove loading message
+        votesList.firstElementChild.remove()
+        //iterate through every new vote and add it to the page
+        for(let i = 0; i < candidates.length; i++) {
+            addNewVote(candidates[i], votes[i])
+        }
+    })
+//Listen for the form to be submitted and add the vote
+votesForm.addEventListener("submit", event => {
+    //Stop form from refreshing the page
+    event.preventDefault()
+    //get new values
+    let newVote = votesForm.elements.vote.value
+    votes.push(newVote)
+    addNewVote(newVote)
+    //reset form
+    votesForm.reset();
+    votesForm.elements.vote.focus()
+})
+
--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const express = require("express");
 const app = express();
 const mysql = require('mysql')
 const alert = require('alert')
+
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b0ec116f629fb6',
+    password: '1b2f0771f9e2812',
+    database: 'heroku_e362e57bba7349a'
+})
+
+
+
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -23,13 +33,7 @@ app.get("/", (request, response) => {
     response.json(dreams);
 });*/
 
-//TODO: Connect to database
-const connection = mysql.createConnection({
-    host: 'us-cdbr-east-02.cleardb.com',
-    user: 'b0ec116f629fb6',
-    password: '1b2f0771f9e2812',
-    database: 'heroku_e362e57bba7349a'
-})
+
 function resetDatabase() {
     //TODO: Fill in method once we know how to connect to the database. Simply run SQL commands to clear the database.
 }
@@ -47,7 +51,7 @@ app.get("/voteFcn", (req, res) => {
     let name = req.query.name
     let nameRegex = /^([A-Z][a-z][0-9][ -]){2,20}/
     if(nameRegex.test(name)) {
-        alert("Invalid input. Name can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
+        window.alert("Invalid input. Name can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
         return;
     }
     //get vote
@@ -63,13 +67,19 @@ app.get("/voteFcn", (req, res) => {
     if(vote==='resetDB') {
         resetDatabase();
         console.log(`Reset database!`)
-        alert("Reset database, please reload the page.")
+        window.alert("Reset database, please reload the page.")
     }
     //TODO: Function to reset database if write-in = resetDB
     //TODO: Query database to insert new record
     console.log(`Received vote from "${name}" for "${vote}".`)
     res.send(`${name}, your vote for ${vote} has been recorded.`)
 });
+let votes = [3,10,4]
+let candidates = ["Nike", "Skechers", "Timbs"]
+app.get('/votes',(req,res)=>{
+    //TODO: Query from database, put results into array below.
+    res.json({nameCandidate:candidates, numVotes:votes})
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
