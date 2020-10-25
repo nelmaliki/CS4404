0fc8f64ccef562a16cfddeb702ba4c2f35fbdc74
matthew7758 <entei678@gmail.com>
2020-10-25 03:12:18 -0400

    Completely reworked code to separate client/server functionality Lag should be gone. TODO: Insert values into database and res.json the proper value upon error.

--- public/script.js
+++ public/script.js
@@ -9,27 +9,58 @@ function addNewVote(candidate, voteNum) {
 }
 
 fetch("/votes")
-    .then(res=>res.json())
+    .then(res => res.json())
     .then(vvvvvvv => {
         let candidates = vvvvvvv.nameCandidate
         let votes = vvvvvvv.numVotes
         //remove loading message
         votesList.firstElementChild.remove()
         //iterate through every new vote and add it to the page
-        for(let i = 0; i < candidates.length; i++) {
+        for (let i = 0; i < candidates.length; i++) {
             addNewVote(candidates[i], votes[i])
         }
     })
 //Listen for the form to be submitted and add the vote
 votesForm.addEventListener("submit", event => {
+    //TODO: get fields, do validation, send to server using restAPI path voteFcn, do a fetch like above and put it in the votes form
+    // fetch input, OBJECT where object is the request of what you want to send, specify what vote you want to add
     //Stop form from refreshing the page
     event.preventDefault()
+
     //get new values
-    let newVote = votesForm.elements.vote.value
-    votes.push(newVote)
-    addNewVote(newVote)
-    //reset form
-    votesForm.reset();
-    votesForm.elements.vote.focus()
+    let voterName = votesForm.elements.name.value
+    let vote = votesForm.elements.vote.value
+    if (vote === 'writein') vote = votesForm.elements.writein.value
+    let nameRegex = /^([A-Z][a-z][0-9][ -]){2,20}/
+    if (nameRegex.test(name)) {//parse inputs
+        //TODO: Move this to client. Send res.json({res:true,message:STRING})
+        window.alert("Invalid input. Name can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
+        return;
+    }
+    if (nameRegex.test(vote) || vote === '') {
+        window.alert("Invalid input. Write can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
+        return;
+    }
+    let stringThing = JSON.stringify({name: voterName, candidate: vote})
+    console.log(stringThing)
+    //Input can now be sent to server
+    return fetch("/voteFcn", {
+        method: "POST",
+        body: JSON.stringify({name: voterName, candidate: vote}),
+        headers: {"Content-Type": "application/json"}
+    })
+        .then(res => res.json())
+        .then(json => {
+            if(json.res ===true)
+                window.alert(json.message)
+            else if(json.res===false){
+                window.alert(json.message)
+                votesForm.reset()
+                votesForm.elements.vote.focus()
+            }
+            //reset form
+            votesForm.reset()
+            votesForm.elements.vote.focus()
+        })
 })
 
--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 const mysql = require('mysql')
-const alert = require('alert')
+const bodyParser = require('body-parser')
 
 const connection = mysql.createConnection({
     host: 'us-cdbr-east-02.cleardb.com',
@@ -16,6 +16,23 @@ const connection = mysql.createConnection({
     database: 'heroku_e362e57bba7349a'
 })
 
+connection.connect(function(err) {
+    if(err)
+        return console.error('error '+err.message)
+    let createVotes = `CREATE TABLE IF NOT EXISTS Votes(voterName VARCHAR(255) NOT NULL, candidateName VARCHAR(255) NOT NULL, PRIMARY KEY(voterName))`
+    connection.query(createVotes,function (err, results, fields) {
+        if(err) {
+            console.log(err.message)
+        }
+    })
+    connection.end(function(err) {
+        if(err) {
+            return console.log(err.message)
+        }
+    })
+})
+
+//TODO: Write code here that creates schema if it does not exist in the database
 
 
 // make all the files in 'public' available
@@ -38,42 +55,30 @@ function resetDatabase() {
     //TODO: Fill in method once we know how to connect to the database. Simply run SQL commands to clear the database.
 }
 
-app.get("/voteFcn", (req, res) => {
+app.post("/voteFcn", bodyParser.json(), (req, res) => {
     //TODO: Ensure voter only votes once. (IP checks?)
-   //Get voter name
-    console.log(req.query)
+    console.log(req.body.toString())
     //test db connection
     connection.connect()
     connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
         if (error) throw error;
         console.log('The solution is: ', results[0].solution);
     });
-    let name = req.query.name
-    let nameRegex = /^([A-Z][a-z][0-9][ -]){2,20}/
-    if(nameRegex.test(name)) {
-        window.alert("Invalid input. Name can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
-        return;
-    }
-    //get vote
-    let vote = req.query.vote
-    if(vote === 'writein'){
-        vote = req.query.writein;
-    }
-    if(nameRegex.test(vote) || vote==='') {
-        alert("Invalid input. Write can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
-        return;
-    }
+    let name = req.body.name
+    let vote = req.body.candidate
+    console.log('NAME = '+name)
+    console.log('VOTE = '+vote)
     //Quick and dirty way to reset the database on the fly. NOTE: REMOVE BEFORE DEPLOYMENT
     if(vote==='resetDB') {
         resetDatabase();
         console.log(`Reset database!`)
-        window.alert("Reset database, please reload the page.")
     }
-    //TODO: Function to reset database if write-in = resetDB
-    //TODO: Query database to insert new record
+    //TODO: Query database to insert new record, res.json a false and new message if the data does not get entered into the database
     console.log(`Received vote from "${name}" for "${vote}".`)
-    res.send(`${name}, your vote for ${vote} has been recorded.`)
+    res.json({res:true,message:`${name}, your vote for ${vote} has been recorded.`})
 });
+
+
 let votes = [3,10,4]
 let candidates = ["Nike", "Skechers", "Timbs"]
 app.get('/votes',(req,res)=>{
