ef9ea4e752ffacb31262ba5f4f4209fc41182c00
matthew7758 <entei678@gmail.com>
2020-10-26 02:00:09 -0400

    Finished voting system Fixed bug causing the list of votes on webpage to not clear on page reload Database can be reset by entering resetDB into write-in field on page Database can be deleted by entering deleteDB into write-in field on page

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-//TODO: Make client-side js that reads json from server-side and puts voting results in a ul in the webpage
 const votesList = document.getElementById("votes")
 const votesForm = document.querySelector("form")
 
@@ -14,7 +13,8 @@ fetch("/votes")
         let candidates = vvvvvvv.nameCandidate
         let votes = vvvvvvv.numVotes
         //remove loading message
-        votesList.firstElementChild.remove()
+        //votesList.firstElementChild.remove()
+        votesList.innerHTML=''
         //iterate through every new vote and add it to the page
         for (let i = 0; i < candidates.length; i++) {
             addNewVote(candidates[i], votes[i])
@@ -22,48 +22,46 @@ fetch("/votes")
     })
 //Listen for the form to be submitted and add the vote
 votesForm.addEventListener("submit", event => {
-    //TODO: get fields, do validation, send to server using restAPI path voteFcn, do a fetch like above and put it in the votes form
-    // fetch input, OBJECT where object is the request of what you want to send, specify what vote you want to add
     //Stop form from refreshing the page
     event.preventDefault()
 
     //get new values
 
-    let ssn = votesForm.elements.ssn.value
-    console.log(ssn)
+    let ssNumber = votesForm.elements.ssn.value
+    console.log(ssNumber)
     let voterName = votesForm.elements.name.value
     let vote = votesForm.elements.vote.value
     if (vote === 'writein') vote = votesForm.elements.writein.value
-    let nameRegex = /^([A-Z][a-z][0-9][ -]){2,20}/
-    if (nameRegex.test(name)) {//parse inputs
-        //TODO: Move this to client. Send res.json({res:true,message:STRING})
-        window.alert("Invalid input. Name can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
+    //Check for valid input before sending to server
+    if (!/^[a-zA-Z0-9][a-zA-Z0-9\s-]*$/.test(voterName)) {//parse inputs
+        alert("Invalid input. Name can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
         return;
     }
-    if (nameRegex.test(vote) || vote === '') {
-        window.alert("Invalid input. Write can only contain A-Z, a-z, 0-9, and spaces/hyphens.")
+    if (!/^[a-zA-Z0-9][a-zA-Z0-9\s-]*$/.test(vote)) {
+        alert(`Invalid input "${vote}". Write-In can only contain A-Z, a-z, 0-9, and spaces/hyphens.`)
         return;
     }
-    let stringThing = JSON.stringify({ssn: ssn, name: voterName, candidate: vote})
-    console.log(stringThing)
+    if (!/^([0-9])*$/.test(ssNumber) || ssNumber === '' || !(ssNumber.length !== '9')) {
+        alert(`Invalid input for SSN "${ssNumber}" with length "${ssNumber.length}". Enter nine digits with no spaces or symbols.`)
+    }
     //Input can now be sent to server
     return fetch("/voteFcn", {
         method: "POST",
-        body: JSON.stringify({ssn: ssn, name: voterName, candidate: vote}),
+        body: JSON.stringify({ssn: ssNumber, name: voterName, candidate: vote}),
         headers: {"Content-Type": "application/json"}
     })
         .then(res => res.json())
         .then(json => {
-            if(json.res ===true)
-                window.alert(json.message)
-            else if(json.res===false){
+            if(json.res ===true) {
                 window.alert(json.message)
                 votesForm.reset()
-                votesForm.elements.vote.focus()
+            }
+            else if(json.res===false){
+                alert(json.message)
+                location.reload()
             }
             //reset form
             votesForm.reset()
-            votesForm.elements.vote.focus()
         })
 })
 
--- server.js
+++ server.js
@@ -16,23 +16,16 @@ const connection = mysql.createConnection({
     database: 'heroku_e362e57bba7349a'
 })
 
-connection.connect(function(err) {
-    if(err)
-        return console.error('error '+err.message)
-    let createVotes = `CREATE TABLE IF NOT EXISTS Votes(SSN varchar(255),voterName VARCHAR(255) NOT NULL, candidateName VARCHAR(255) NOT NULL, PRIMARY KEY(SSN))`
-    connection.query(createVotes,function (err, results, fields) {
-        if(err) {
+connection.connect(function (err) {
+    if (err)
+        return console.error('error ' + err.message)
+    let createVotes = `CREATE TABLE IF NOT EXISTS Votes(SSN INTEGER(255),voterName VARCHAR(255) NOT NULL, candidateName VARCHAR(255) NOT NULL, PRIMARY KEY(SSN))`
+    connection.query(createVotes, function (err, results, fields) {
+        if (err) {
             console.log(err.message)
         }
     })
-    connection.end(function(err) {
-        if(err) {
-            return console.log(err.message)
-        }
-    })
 })
-
-//TODO: Write code here that creates schema if it does not exist in the database
 
 
 // make all the files in 'public' available
@@ -50,72 +43,92 @@ app.get("/", (request, response) => {
     response.json(dreams);
 });*/
 
-
+//Call by putting resetDB in the write-in field
 function resetDatabase() {
-    //TODO: Fill in method once we know how to connect to the database. Simply run SQL commands to clear the database.
+    let deleteRecords = `DELETE FROM Votes;`
+    connection.query(deleteRecords, function (err, results, fields) {
+        if (err) {
+            return console.log(err.message)
+        }
+    })
+}
 
-    connection.connect(function(err) {
-        if(err)
-            return console.error('error '+err.message)
-        let deleteRecords = `DELETE FROM Votes;`
-        connection.query(deleteRecords,function (err, results, fields) {
-            if(err) {
-                console.log(err.message)
-            }
-        })
-        connection.end(function(err) {
-            if(err) {
-                return console.log(err.message)
-            }
-        })
+//Call by putting deleteDB in the write-in field
+function deleteDatabase() {
+    let deleteDB = `DROP TABLE Votes`
+    connection.query(deleteDB, function (err, results, fields) {
+        if (err) {
+            return console.log(err.message)
+        }
     })
 }
 
 app.post("/voteFcn", bodyParser.json(), (req, res) => {
-    //TODO: Ensure voter only votes once. (IP checks?)
-    console.log(req.body.toString())
-    //test db connection
-    connection.connect()
-    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
-    });
+    console.log(req.body)
     let ssn = req.body.ssn
     let name = req.body.name
     let vote = req.body.candidate
-    console.log('SSN = '+ssn)
-    console.log('NAME = '+name)
-    console.log('VOTE = '+vote)
+    /*console.log('SSN = ' + ssn)
+    console.log('NAME = ' + name)
+    console.log('VOTE = ' + vote)*/
 
-    addToDataBase(ssn,name,vote)
-    //Quick and dirty way to reset the database on the fly. NOTE: REMOVE BEFORE DEPLOYMENT
-    if(vote==='resetDB') {
-        resetDatabase();
-        console.log(`Reset database!`)
+    //Quick and dirty way to reset/delete the database on the fly. NOTE: REMOVE BEFORE DEPLOYMENT
+    if (vote === 'resetDB') {
+        resetDatabase()
+        res.json({res: false, message: `Reset database.`})
+        return console.log(`Reset database!`)
     }
-    //TODO: Query database to insert new record, res.json a false and new message if the data does not get entered into the database
-    console.log(`Received vote from "${name}" for "${vote}".`)
-    res.json({res:true,message:`${name}, your vote for ${vote} has been recorded.`})
+    if (vote === 'deleteDB') {
+        deleteDatabase()
+        res.json({res: false, message: `Deleted database, please restart the server.`})
+        return console.log('Deleted database, please restart server.')
+    }
+    let value = 0
+    addToDatabase({ssn,name,vote},function(result) {
+        value = result
+        console.log(`VALUE = ${value}`)
+        if (value) {
+            console.log("addToDatabase returned true.")
+
+            res.json({res: true, message: `${name}, your vote for ${vote} has been recorded.`})
+        } else {
+            console.log("addToDatabase returned false.")
+            res.json({res: false, message: `${name}, you have already voted.`})
+        }
+    })
 });
 
-function addToDataBase(ssn,name,vote){
-    con.connect(function(err) {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = `INSERT INTO Votes  VALUES (${ssn}, ${name},${vote})`;
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("1 record inserted");
-        });
-    });
+function addToDatabase(data, callback) {
+    let sql = `INSERT INTO Votes VALUES (${data.ssn}, '${data.name}','${data.vote}')`;
+    connection.query(sql, function (err, results, fields) {
+        if (err) {
+            console.log(err.message)
+            return callback(0)
+
+        }
+        console.log("1 record inserted.")
+        return callback(1)
+    })
 }
 
 
-let votes = [3,10,4]
-let candidates = ["Nike", "Skechers", "Timbs"]
-app.get('/votes',(req,res)=>{
-    //TODO: Query from database, put results into array below.
-    res.json({nameCandidate:candidates, numVotes:votes})
+
+app.get('/votes', (req, res) => {
+    let candidates = []
+    let votes = []
+    let sql = `SELECT candidateName, COUNT(*) AS numVotes FROM VOTES GROUP BY candidateName`
+    connection.query(sql, function (err, results) {
+        if (err) {
+            console.log(`Error in app.get query`)
+            return console.log(err.message)
+        }
+        Object.keys(results).forEach(function (key) {
+            var row = results[key]
+            candidates.push(row.candidateName)
+            votes.push(row.numVotes)
+        })
+        res.json({nameCandidate: candidates, numVotes: votes})
+    })
 })
 
 // listen for requests :)
